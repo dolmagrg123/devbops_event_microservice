ee09a04b0721474d19298cfba555cc849c7d7caa
francisrega <francis.regalado1@gmail.com>
2020-10-02 20:13:39 -0400

    features added

--- Events.py
+++ Events.py
@@ -13,13 +13,14 @@ def posting():
 @app.route("/event/rsvp")
 def comment():
     t1.put("Event_name", "Event_desc", "Date_time", "Price", "Address", "Virtual")
-#     comment = request.form['Comment']
-#     eventID = request.form['eventID']
-#     #each event contains a unique ID
-#     status = aws.saveComment(eventID, comment)
-#     #if (status == -1) return "Failed"
-#     #status :either true or false to show
-#     return status #commentID
+
+@app.route("/event/diff feature")
+def comment():
+    t1.put("Event_name", "Event_desc", "Date_time", "Price", "Address", "Virtual")
+
+@app.route("/event/diff feature")
+def comment():
+    t1.put("Event_name", "Event_desc", "Date_time", "Price", "Address", "Virtual")
 
 # @app.route("/viewing", methods= ["GET"])
 # def viewing():
