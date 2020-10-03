fcaaf0e463bda8778c4d61b44eda87bc479b5e37
francisrega <francis.regalado1@gmail.com>
2020-10-03 10:14:26 -0400

    commented out some code

--- Events.py
+++ Events.py
@@ -10,9 +10,9 @@ def posting():
     t1.put("Event_name", "Event_date", "Event_time", "User", "Event_desc", "Event_image", "Event_location")
     
 
-@app.route("/event/rsvp", methods=["POST"])
-def comment():
-     t1.put("Event_name", "Event_date", "Event_time", "User", "Event_desc", "Event_image", "Event_location")
+# @app.route("/event/rsvp", methods=["POST"])
+# def rsvp():
+#      t1.put("Event_name", "Event_date", "Event_time", "User", "Event_desc", "Event_image", "Event_location")
 
 # @app.route("/event/diff_feature")
 # def comment():
