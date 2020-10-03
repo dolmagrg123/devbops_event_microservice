2f4418bfb53314f70c2fb6c6ff8e1a48986d2b61
francisrega <francis.regalado1@gmail.com>
2020-10-03 12:38:57 -0400

    added features to implement

--- Events.py
+++ Events.py
@@ -6,26 +6,25 @@ app = Flask(__name__)
 t1 = Events()
 
 @app.route("/event")
-def posting():
-    t1.put("Event_name", "Event_date", "Event_time", "User", "Event_desc", "Event_image", "Event_location")
-    
+def create():
+    t1.put(Event_name = "Presentation", Event_date = "October 3rd", Event_time = "2pm", User = "Class", Event_desc = "Presenting things", Event_image = "None", Event_location = "Zoom")
+
+# @app.route("/event/viewing", methods= ["GET"])
+# def view_event():
+#     return 
+
+# @app.route("/event/update")
+# def update_event():
+#     t1.put("Event_name", "Event_desc", "Date_time", "Price", "Address", "Virtual")
+
+
+# @app.route("/event/delete", methods=["POST"])
+# def delete_eventç():
+#      t1.put("Event_name", "Event_date", "Event_time", "User", "Event_desc", "Event_image", "Event_location")
 
 # @app.route("/event/rsvp", methods=["POST"])
 # def rsvp():
 #      t1.put("Event_name", "Event_date", "Event_time", "User", "Event_desc", "Event_image", "Event_location")
-
-# @app.route("/event/diff_feature")
-# def comment():
-#     t1.put("Event_name", "Event_desc", "Date_time", "Price", "Address", "Virtual")
-
-# @app.route("/event/diff_feature")
-# def comment():
-#     t1.put("Event_name", "Event_desc", "Date_time", "Price", "Address", "Virtual")
-
-# @app.route("/viewing", methods= ["GET"])
-# def viewing():
-#     comment = aws.getAllPost()
-#     return content
 
 if __name__ == '__main__':
     app.run(debug=True)
