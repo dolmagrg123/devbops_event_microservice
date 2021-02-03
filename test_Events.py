e47fd54fd6543aa60a557bda63ccb95af771fc6b
dolmagrg123 <62584166+dolmagrg123@users.noreply.github.com>
2021-02-03 10:10:51 -0500

    Update test_Events.py

--- test_Events.py
+++ test_Events.py
@@ -23,52 +23,52 @@ class BasicTestCase(unittest.TestCase):
           #print(data['Status'], data['Error'])
           assert data['Result'] == True
 
-#       def test_2_creating(self):
-#           req = {
-#               'Event_name': "QA_TEST",
-#               'Event_date': date.today().strftime("%B %d, %Y"),
-#               'Event_time': datetime.now(),
-#               'User': "username",
-#               'Event_desc': "Event_desc",
-#               'Event_image': "Event_image",
-#               'Event_location': "Event_location",
-#               'Online': "Online"
-#         }
+      def test_2_creating(self):
+          req = {
+              'Event_name': "QA_TEST",
+              'Event_date': date.today().strftime("%B %d, %Y"),
+              'Event_time': datetime.now(),
+              'User': "username",
+              'Event_desc': "Event_desc",
+              'Event_image': "Event_image",
+              'Event_location': "Event_location",
+              'Online': "Online"
+        }
 
-#           rv = self.app.post('/create-event', json=req)
-#           data = json.loads(rv.data)
-#           #print(data['Status'], data['Error'])
-#           print(data)
-#           assert data['Result'] == True
+          rv = self.app.post('/create-event', json=req)
+          data = json.loads(rv.data)
+          #print(data['Status'], data['Error'])
+          print(data)
+          assert data['Result'] == True
 
-#       def test_3_update(self):
-#           req = {
-#               'Event_name':  "QA_TEST",
-#               'New_Event_date': date.today().strftime("%B %d, %Y"),
-#               'New_Event_time': datetime.now(),
-#               'New_User': "username",
-#               'New_Event_desc': "Event_desc",
-#               'New_Event_image': "Event_image",
-#               'New_Event_location': "Event_location",
-#               'New_Online': "Online"
-#         }
+      def test_3_update(self):
+          req = {
+              'Event_name':  "QA_TEST",
+              'New_Event_date': date.today().strftime("%B %d, %Y"),
+              'New_Event_time': datetime.now(),
+              'New_User': "username",
+              'New_Event_desc': "Event_desc",
+              'New_Event_image': "Event_image",
+              'New_Event_location': "Event_location",
+              'New_Online': "Online"
+        }
 
-#           rv = self.app.post('/event-update', json=req)
-#           data = json.loads(rv.data)
-#           #print(data['Status'], data['Error'])
-#           print(data)
-#           assert data['Result'] == True
+          rv = self.app.post('/event-update', json=req)
+          data = json.loads(rv.data)
+          #print(data['Status'], data['Error'])
+          print(data)
+          assert data['Result'] == True
 
-#       def test_4_delete(self):
-#           req = {
-#               'Event_name': "QA_TEST"
-#         }
+      def test_4_delete(self):
+          req = {
+              'Event_name': "QA_TEST"
+        }
 
-#           rv = self.app.post('/event-delete', json=req)
-#           data = json.loads(rv.data)
-#           #print(data['Status'], data['Error'])
-#           print(data)
-#           assert data['Result'] == True
+          rv = self.app.post('/event-delete', json=req)
+          data = json.loads(rv.data)
+          #print(data['Status'], data['Error'])
+          print(data)
+          assert data['Result'] == True
 
 if __name__ == '__main__':
     unittest.main()
