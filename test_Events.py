73497971cd209f741c58e505ebc43d6b8fb73561
Dolma Gurung <62584166+dolmagrg123@users.noreply.github.com>
2021-02-06 15:11:51 -0500

    Fixed Test

--- test_Events.py
+++ test_Events.py
@@ -87,7 +87,7 @@ class BasicTestCase(unittest.TestCase):
           data = json.loads(rv.data)
           #print(data['Status'], data['Error'])
           print(data)
-          assert data['Result'] == False
+          assert data['Result'] == True
 
 if __name__ == '__main__':
     unittest.main()
